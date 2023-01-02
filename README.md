c4c07788cfd6c7306141bb7a0e3e4230193c0bec
Sherlock Holmes <bazil.lucky@gmail.com>
2023-01-02 13:35:49 -0500

    tutorial files added to the repo

---
+++ README.md
@@ -0,0 +1,9 @@
+# flutter_amplified_todo_app
+
+This is a Flutter project built using [AWS Amplify tutorial](https://docs.amplify.aws/start/getting-started/installation/q/integration/flutter/)
+
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyfluttertest": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifyfluttertest": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
